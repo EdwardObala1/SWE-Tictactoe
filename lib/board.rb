da173bc9d2d2f3a49bb46573e04cae5dffbf8c56
EdwardObala1 <e.obala@alustudent.com>
2021-05-12 12:35:42 +0200

    refactoring from using a global state to an instance variable

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,18 @@
 class Board
+  $grid_with_moves=Array[
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ]
   def grid
-    [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-      ]
+    return $grid_with_moves
   end
 
-  def place_move(value,row,column)
-    [
-        ["", "", ""],
-        ["", "", "X"],
-        ["", "", ""]
-      ]
+  def self.place_move(value,row,column)
+    
+    if value != 0 and row != 0 and column != 0
+      $grid_with_moves[row-1][column-1]=value
+    end
+    return $grid_with_moves
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Board do
 
   it 'starts with an empty grid' do
     board = Board.new
-
     expect(board.grid).to eq([
       ["", "", ""],
       ["", "", ""],
@@ -13,12 +12,36 @@ RSpec.describe Board do
   end
 
   it 'marks a position on the grid' do
-    board = Board.new
-    expect(board.place_move("X",2,3)).to eq([
+    expect(Board.place_move("X",2,3)).to eq([
         ["", "", ""],
         ["", "", "X"],
         ["", "", ""]
       ])
   end
-  
+
+  it 'marks a position on the grid' do
+    board = Board.new
+    Board.place_move("X", 2, 2)
+    Board.place_move("O", 1, 2)
+
+    expect(board.grid).to eq([
+        ["", "O", ""],
+        ["", "X", "X"],
+        ["", "", ""]
+      ])
+  end
+
+  it 'marks a position on the grid' do
+    board = Board.new
+    Board.place_move("X", 1, 3)
+    Board.place_move("O", 3, 3)
+    
+
+    expect(board.grid).to eq([
+        ["", "O", "X"],
+        ["", "X", "X"],
+        ["", "", "O"]
+      ])
+  end
+
 end
