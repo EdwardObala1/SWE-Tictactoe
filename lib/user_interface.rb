d574dd9d8faef1cb600ce4c98f0500c9535c1b5f
EdwardObala1 <e.obala@alustudent.com>
2021-05-19 13:07:24 +0200

    cleaned code, and new tests on prompting the player for theri plays, play position on the grid and getting their details (name)

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,10 +1,28 @@
 require 'stringio'
+require 'board'
 
 class UserInterface
+  attr_accessor :row,:column
 
-  def get_user_name(output, input)
-    output.puts 'Please enter your name:'
-    
-    input.gets.chomp
+  def initialize(output, input)
+    @output = output
+    @input = input
+  end
+
+  def get_user_name
+    @output.puts 'Please enter your name:'
+    @input.gets.chomp
+  end
+
+  def prompt_next_player(mark)
+    @output.puts "Player #{mark} goes next"
+  end
+
+  def get_play_position
+    @output.puts "Put in your Row and Column"
+    row = @input.gets.chomp.to_i
+    column = @input.gets.chomp.to_i
+
+    [row, column]
   end
 end
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -2,15 +2,34 @@ require 'user_interface'
 require 'stringio'
 
 RSpec.describe UserInterface do
+  let(:output) { StringIO.new }
   
   it 'asks the user for their name' do
-    ui = UserInterface.new
-    output = StringIO.new
     input = StringIO.new("Alice\n")
-    
-    user_name = ui.get_user_name(output, input)
+    ui = UserInterface.new(output, input)
+
+    user_name = ui.get_user_name
 
     expect(output.string).to eq("Please enter your name:\n")
     expect(user_name).to eq("Alice")
+  end
+
+  it "prints out who's player's turn it is" do
+    ui = UserInterface.new(output, StringIO.new)
+
+    ui.prompt_next_player("X")
+
+    expect(output.string).to eq("Player X goes next\n")
+  end
+
+  it 'Get the row and Column the user want to make a play in' do
+    output = StringIO.new
+    input = StringIO.new("2\n3\n")
+    ui = UserInterface.new(output,input)
+
+    position_of_play = ui.get_play_position
+
+    expect(output.string).to eq ("Put in your Row and Column\n")
+    expect(position_of_play).to eq([2, 3])
   end
 end
